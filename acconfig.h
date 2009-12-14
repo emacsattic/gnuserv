be3f7c89ef43a7c02f0120e86e25ad772e0020f5
Jan Vroonhof <vroonhof@math.ethz.ch>
2009-12-14 02:23:26 +0100

    Mon Dec 14 02:23:26 CET 2009 update

---
+++ acconfig.h
@@ -0,0 +1,20 @@
+/* Define this if SYSV IPC is available (e.g. msgget, etc.) */
+#undef HAVE_SYSVIPC
+
+/* Define this if internet-domain sockets are available */
+#undef HAVE_INTERNET_DOMAIN_SOCKETS
+
+/* Define this if berkeley unix-domain sockets are available */
+#undef HAVE_UNIX_DOMAIN_SOCKETS
+
+/* Define this if have sun_len member in struct sockaddr_un */
+#undef HAVE_SOCKADDR_SUN_LEN
+
+/* Define this if using Xauth authentication */
+#undef HAVE_XAUTH
+
+#undef socklen_t
+
+#undef HAVE_X_WINDOWS
+
+#undef _GNU_SOURCE
